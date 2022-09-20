8ca7ea32144fbc5b508e44194abc4a4e2f286fd8
cubikean <hugochatigny@gmail.com>
2022-09-20 17:58:42 +0200

    update

--- masteringnuxt.py
+++ masteringnuxt.py
@@ -1,5 +1,5 @@
 from url_downloader import save_file
 
-for i in range(54,118):
+for i in range({VALUE}):
     
     save_file(url="https://api.masteringnuxt.com/lessons/"+str(i)+"/download/hd?api_token={TOKEN}", file_path='C:/Users/user/Desktop/{PATH}', file_name=str(i)+'.mp4')
