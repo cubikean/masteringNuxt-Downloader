a2c22308f2d667d1ecfbdb5f23ffb7054d5e7714
cubikean <hugochatigny@gmail.com>
2022-09-20 17:26:47 +0200

    ADD script

---
+++ masteringnuxt.py
@@ -0,0 +1,5 @@
+from url_downloader import save_file
+
+for i in range(54,118):
+    
+    save_file(url="https://api.masteringnuxt.com/lessons/"+str(i)+"/download/hd?api_token={TOKEN}", file_path='C:/Users/user/Desktop/{PATH}', file_name=str(i)+'.mp4')
